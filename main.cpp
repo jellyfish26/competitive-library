4449dc138b5252353141ec71bf0b31bde26c51ce
jellyfish26 <jellyfish0162@gmail.com>
2019-03-14 18:18:31 +0900

    Migrate from CLion to VSCode

--- main.cpp
+++ main.cpp
@@ -5,9 +5,16 @@ int MOD = (int)1e9 + 7;
 int INF = (int)2e18;
 // Comment out below and program paste
 
-#include "UnionFind.h"
-#include "power.h"
+#include "UnionFind.hpp"
+#include "power.hpp"
 
 signed main() {
-    cout << power(3, 1000, MOD) << endl;
+    int L;
+    cin >> L;
+    for (int i = 0; i < 50; i++) {
+        for (int j = 0; j < 50; j++) {
+            cout << "Hello World" << endl;
+        }
+    }
+    
 }
