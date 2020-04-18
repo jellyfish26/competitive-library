d95d9e6b3821064eba9ebc0266ba43b0741b1ee5
jellyfish26 <jellyfish0162@gmail.com>
2020-04-18 17:03:28 +0900

    modify UnionFind

--- structure/UnionFind.cpp
+++ structure/UnionFind.cpp
@@ -1,29 +1,27 @@
-#include <cstdio>
-#define int long long
+#include <bits/stdc++.h>
 using namespace std;
 
+// Last updated 2020-4-18 (Disjoint set), ABC120-D Decayed Bridges
 struct UnionFind {
     vector<int> data;
 
-    explicit UnionFind(int data_size) {
-        data.assign(static_cast<unsigned long>(data_size), -1);
-    }
+    explicit UnionFind(int size) : data(size, -1)  {}
 
     bool unite(int x, int y) {
         x = find(x), y = find(y);
-        if(x == y) return (false);
+        if(x == y) return false;
         if(data[x] > data[y]) swap(x, y);
         data[x] += data[y];
         data[y] = x;
-        return (true);
+        return true;
     }
 
-    int find(int a) {
-        if (data[a] < 0) return (a);
-        else return (data[a] = find(data[a]));
+    int find(int idx) {
+        if (data[idx] < 0) return idx;
+        else return (data[idx] = find(data[idx]));
     }
 
-    int size(int a) {
-        return (-data[find(a)]);
+    int size(int idx) {
+        return (-data[find(idx)]);
     }
 };
