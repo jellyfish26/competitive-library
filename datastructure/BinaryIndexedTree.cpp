b6cc73619a3801d98772d238a967b7b52dd3b584
jellyfish26 <jellyfish0162@gmail.com>
2020-04-20 15:39:18 +0900

    add BinaryIndexedTree

---
+++ datastructure/BinaryIndexedTree.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Last updated 2020-4-20
+template<typename T>
+struct BinaryIndexedTree {
+    vector<T> data;
+
+    explicit BinaryIndexedTree(int size) : data(size + 1, 0) {}
+
+    void update(int idx, T value) {
+        for (int i = idx + 1; i < data.size(); i += (i & -i)) data[i] += value;
+    }
+
+    T sum(int idx) {
+        T count = 0;
+        for (int i = idx + 1; i > 0; i -= (i & -i)) count += data[i];
+        return count;
+    }
+
+    T sum_all() {
+        return sum(data.size() - 1);
+    }
+
+    // [left, right)
+    T query(int leftIdx, int rightIdx) {
+        return sum(rightIdx - 1) - sum(leftIdx - 1);
+    }
+};
