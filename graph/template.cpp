c67bd65e7a5f2d3c3f6b65fd29a30e74685fc7d4
jellyfish26 <jellyfish0162@gmail.com>
2020-04-21 21:42:12 +0900

    WeightedGraph can inherit from UnWeightedGraph. (Because it seemed easier to implement it in the future.)

--- graph/template.cpp
+++ graph/template.cpp
@@ -13,26 +13,27 @@ struct edge {
         arrow.second = vertex;
         return *this;
     }
+
+    operator int() const { return arrow.second; }
 };
 
 template<typename T>
-struct WeightedGraph : vector<vector<edge<T>>> {
+struct UnWeightedGraph : vector<vector<edge<T>>> {
     bool is_directed_graph;
-    explicit WeightedGraph(int size, bool is_directed_graph) : vector<vector<edge<T>>>(size), is_directed_graph(is_directed_graph) {}
+    explicit UnWeightedGraph(int size, bool is_directed_graph) : vector<vector<edge<T>>>(size), is_directed_graph(is_directed_graph) {}
 
-    void add_edge(int source, int to, T cost) {
-        vector<vector<edge<T>>>::operator[](source).emplace_back(to, cost);
-        if (!is_directed_graph) vector<vector<edge<T>>>::operator[](to).emplace_back(source, cost);
+    void add_edge(int source, int to) {
+        vector<vector<edge<T>>>::operator[](source).emplace_back(to, 0);
+        if (!is_directed_graph) vector<vector<edge<T>>>::operator[](to).emplace_back(source, 0);
     }
 };
 
 template<typename T>
-struct UnWeightedGraph : vector<vector<int>> {
-    bool is_directed_graph;
-    explicit UnWeightedGraph(int size, bool is_directed_graph) : vector<vector<int>>(size), is_directed_graph(is_directed_graph) {}
+struct WeightedGraph : UnWeightedGraph<T> {
+    explicit WeightedGraph(int size, bool is_directed_graph) : UnWeightedGraph<T>(size, is_directed_graph) {}
 
-    void add_edge(int source, int to) {
-        vector<vector<int>>::operator[](source).push_back(to);
-        if (!is_directed_graph) vector<vector<int>>::operator[](to).push_back(source);
+    void add_edge(int source, int to, T cost) {
+        UnWeightedGraph<T>::operator[](source).emplace_back(to, cost);
+        if (!UnWeightedGraph<T>::is_directed_graph) UnWeightedGraph<T>::operator[](to).emplace_back(source, cost);
     }
 };
