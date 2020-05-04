4be58b34407187d2b802ec2424e9b873af9bf88e
jellyfish26 <jellyfish0162@gmail.com>
2020-05-04 15:38:50 +0900

    add LongestIncreaseingSubsequenceTree

---
+++ custom/extension_graph/LongestIncreaseingSubsequenceTree.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../../graph/EulerTour.cpp"
+#include "../../custom/extension_algorithm/UndoableLongestIncreaseingSubsequence.cpp"
+
+// Last-Update 2020-05-04
+template<typename T>
+class LongestIncreaseingSubsequenceTree {
+private:
+
+    UnWeightedGraph<T> tree;
+    EulerTour<T> euler_tour;
+    vector<T> vertex_number_data;
+    vector<size_t> vertex_lis_size;
+    UndoableLongestIncreaseingSubsequence<T> count_lis;
+
+    void dfs() {
+        for (size_t index = 0; index < euler_tour.size(); index++) {
+            size_t now_vertex = euler_tour[index];
+            size_t pre_order, post_order;
+            tie(pre_order, post_order) = euler_tour.index_of_vertex(now_vertex);
+            post_order--;
+            if (pre_order == index) {
+                vertex_lis_size[now_vertex] = count_lis.update(vertex_number_data[now_vertex]);
+            }
+            if (post_order == index) {
+                count_lis.undo();
+            }
+        }
+    }
+
+public:
+    explicit LongestIncreaseingSubsequenceTree(size_t vertex_size, bool is_narrowly_sense) 
+        : tree(vertex_size, false), euler_tour(tree), vertex_number_data(vertex_size, 0), vertex_lis_size(vertex_size) , count_lis(is_narrowly_sense) {}
+    
+    void add_edge(size_t source, size_t to) { tree.add_edge(source, to); }
+
+    void init_vertex(size_t vertex, T number) { vertex_number_data[vertex] = number; }
+
+    void build(size_t start_vertex = 0) {
+        euler_tour.build(start_vertex);
+        dfs();
+    }
+
+    const size_t &operator[] (size_t vertex) { return vertex_lis_size[vertex]; }
+};
