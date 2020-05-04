37613c071f49a12350337348f6c8cf23ea067bba
jellyfish26 <jellyfish0162@gmail.com>
2020-05-04 15:22:59 +0900

    updat graph template

--- graph/template.cpp
+++ graph/template.cpp
@@ -1,30 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Last updated 2020-4-30
+// Last updated 2020-05-04
 template<typename T>
 struct edge {
-    pair<int, int> arrow;
+    pair<size_t, size_t> arrow;
     T cost;
 
-    edge(int source, int to, T cost) : arrow(source, to), cost(cost) {}
-    edge(int to, T cost) : arrow(-1, to), cost(cost) {}
+    edge(size_t source, size_t to, T cost) : arrow(source, to), cost(cost) {}
+    edge(size_t to, T cost) : arrow(-1, to), cost(cost) {}
 
-    edge &operator=(const int &vertex) {
+    edge &operator=(const size_t &vertex) {
         arrow.second = vertex;
         return *this;
     }
 
-    operator int() const { return arrow.second; }
+    operator size_t() const { return arrow.second; }
 };
 
 template<typename T>
 struct UnWeightedGraph {
-    bool is_directed_graph;
     vector<vector<edge<T>>> data;
+    bool is_directed_graph;
+
     explicit UnWeightedGraph(size_t size, bool is_directed_graph) : data(size), is_directed_graph(is_directed_graph) {}
 
-    void add_edge(int source, int to) {
+    void add_edge(size_t source, size_t to) {
         data[source].emplace_back(to, 0);
         if (!is_directed_graph) data[to].emplace_back(source, 0);
     }
@@ -38,7 +39,7 @@ template<typename T>
 struct WeightedGraph : UnWeightedGraph<T> {
     explicit WeightedGraph(size_t size, bool is_directed_graph) : UnWeightedGraph<T>(size, is_directed_graph) {}
 
-    void add_edge(int source, int to, T cost) {
+    void add_edge(size_t source, size_t to, T cost) {
         UnWeightedGraph<T>::operator[](source).emplace_back(to, cost);
         if (!UnWeightedGraph<T>::is_directed_graph) UnWeightedGraph<T>::operator[](to).emplace_back(source, cost);
     }
