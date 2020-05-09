a7a361ce52d237ebeee15dec49f9b98e4fe8b087
jellyfish26 <jellyfish0162@gmail.com>
2020-05-09 23:33:53 +0900

    update Dijkstra

--- graph/Dijkstra.cpp
+++ graph/Dijkstra.cpp
@@ -2,39 +2,39 @@
 #include "template.cpp"
 using namespace std;
 
-// Last updated 2020-4-30
+// Last updated 2020-05-09
 template<typename T>
 class Dijkstra {
 private:
     WeightedGraph<T> &graph;
     vector<vector<T>> shortest_distance;
 
-    void init_and_exec_dijkstra(size_t start_vertex, const T MAX_INF, function<bool(int, T)> update_function) {
+    void init_and_exec_dijkstra(int start_vertex, const T MAX_INF, function<bool(int, T)> update_function) {
         shortest_distance[start_vertex] = vector<T>(graph.size(), MAX_INF);
         exec_dijkstra(start_vertex, update_function, shortest_distance[start_vertex]);
     }
 
-    void default_dijkstra(size_t start_vertex, const T MAX_INF) {
+    void default_dijkstra(int start_vertex, const T MAX_INF) {
         init_and_exec_dijkstra(start_vertex, MAX_INF, [&](size_t next_vertex, T next_cost) {
             return shortest_distance[start_vertex][next_vertex] == next_cost;
         });
     }
 
 protected:
-    void exec_dijkstra(size_t start_vertex, function<bool(size_t, T)> update_function, vector<T> &now_distance) {
-        priority_queue<pair<T, size_t>, vector<pair<T, size_t>>, greater<>> next_edges;
+    void exec_dijkstra(int start_vertex, function<bool(int, T)> update_function, vector<T> &now_distance) {
+        priority_queue<pair<T, int>, vector<pair<T, int>>, greater<>> next_edges;
         now_distance[start_vertex] = 0;
         next_edges.emplace(now_distance[start_vertex], start_vertex);
         while (!next_edges.empty()) {
-            size_t now_vertex;
+            int now_vertex;
             T now_cost;
             tie(now_cost, now_vertex) = next_edges.top();
             next_edges.pop();
             if (now_distance[now_vertex] < now_cost) continue;
             for (auto &next_vertex : graph[now_vertex]) {
                 T next_cost = now_cost + next_vertex.cost;
-                if (now_distance[next_vertex.arrow.second] < next_cost) continue;
-                if (update_function(next_vertex.arrow.second, next_cost)) continue;
+                if (now_distance[next_vertex.to] < next_cost) continue;
+                if (update_function(next_vertex.to, next_cost)) continue;
                 now_distance[next_vertex] = next_cost;
                 next_edges.emplace(next_cost, next_vertex);
             }
@@ -42,19 +42,19 @@ protected:
     }
 
 public:
-    explicit Dijkstra(WeightedGraph<T> &graph) : graph(graph), shortest_distance(graph.size()) {}
+    Dijkstra(WeightedGraph<T> &graph) : graph(graph), shortest_distance(graph.size()) {}
 
-    T query_overwrite(size_t start_vertex, size_t end_vertex, const T MAX_INF) {
+    T query_overwrite(int start_vertex, int end_vertex, const T MAX_INF) {
         default_dijkstra(start_vertex, MAX_INF);
         return shortest_distance[start_vertex][end_vertex];
     }
 
-    T query(size_t start_vertex, size_t end_vertex, const T MAX_INF)  {
+    T query(int start_vertex, int end_vertex, const T MAX_INF)  {
         if (shortest_distance[start_vertex].empty()) default_dijkstra(start_vertex, MAX_INF);
         return shortest_distance[start_vertex][end_vertex];
     }
 
-    void query(size_t start_vertex, const T MAX_INF) { if (shortest_distance[start_vertex].empty()) default_dijkstra(start_vertex, MAX_INF); }
+    void query(int start_vertex, const T MAX_INF) { if (shortest_distance[start_vertex].empty()) default_dijkstra(start_vertex, MAX_INF); }
 
     // read only
     constexpr vector<T> &operator[] (const size_t start_vertex) const { return shortest_distance[start_vertex]; }
