4d289cc7cf1b8af03033510e210f09e508ea3ad8
jellyfish26 <jellyfish0162@gmail.com>
2020-05-09 20:56:19 +0900

    add Kruskal

---
+++ graph/Kruskal.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include "template.cpp"
+#include "../datastructure/UnionFind.cpp"
+using namespace std;
+
+// Last updated 2020-05-09
+template<typename T>
+class Kruskal {
+private:
+    const size_t vertex_size;
+    vector<edge<T>> edges, use_edges;
+    T kruskal_cost;
+
+    void convert_graph_to_edges(WeightedGraph<T> &graph) {
+        for (auto &vertex_edges : graph) {
+            for (auto &temp : vertex_edges) edges.push_back(temp);
+        }
+    }
+
+public:
+    Kruskal(WeightedGraph<T> &graph) : vertex_size(graph.size()) {
+        convert_graph_to_edges(graph);
+    }
+
+    Kruskal(vector<edge<T>> &edges, size_t vertex_size) : vertex_size(vertex_size), edges(edges) {}
+
+    void build() {
+        kruskal_cost = 0;
+        sort(edges.begin(), edges.end(), [](edge<T> left, edge<T> right) {
+            return left.cost < right.cost;
+        });
+        UnionFind uf(vertex_size);
+        for (auto &now_edge : edges) {
+            if (uf.unite(now_edge.source, now_edge.to)) {
+                kruskal_cost += now_edge.cost;
+                use_edges.push_back(now_edge);
+            }
+        }
+    }
+
+    T query() { return kruskal_cost; }
+
+    vector<edge<T>> &get_use_edges() { return use_edges; }
+};
