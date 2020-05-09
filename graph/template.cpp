ba436d556a391fad3bf6b674d1609b2c1845a86b
jellyfish26 <jellyfish0162@gmail.com>
2020-05-09 18:36:02 +0900

    add LowestCommonAncestorDoubling

--- graph/template.cpp
+++ graph/template.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Last updated 2020-05-04
+// Last updated 2020-05-09
 template<typename T>
 struct edge {
-    pair<size_t, size_t> arrow;
+    int source, to;
     T cost;
 
-    edge(size_t source, size_t to, T cost) : arrow(source, to), cost(cost) {}
-    edge(size_t to, T cost) : arrow(-1, to), cost(cost) {}
+    edge(int source, int to, T cost) : source(source), to(to), cost(cost) {}
+    edge(int to, T cost) : source(-1), to(to), cost(cost) {}
 
-    edge &operator=(const size_t &vertex) {
-        arrow.second = vertex;
+    edge &operator=(const int &vertex) {
+        to = vertex;
         return *this;
     }
 
-    operator size_t() const { return arrow.second; }
+    operator int() const { return to; }
 };
 
 template<typename T>
@@ -25,7 +25,7 @@ struct UnWeightedGraph {
 
     explicit UnWeightedGraph(size_t size, bool is_directed_graph) : data(size), is_directed_graph(is_directed_graph) {}
 
-    void add_edge(size_t source, size_t to) {
+    void add_edge(int source, int to) {
         data[source].emplace_back(to, 0);
         if (!is_directed_graph) data[to].emplace_back(source, 0);
     }
@@ -39,7 +39,7 @@ template<typename T>
 struct WeightedGraph : UnWeightedGraph<T> {
     explicit WeightedGraph(size_t size, bool is_directed_graph) : UnWeightedGraph<T>(size, is_directed_graph) {}
 
-    void add_edge(size_t source, size_t to, T cost) {
+    void add_edge(int source, int to, T cost) {
         UnWeightedGraph<T>::operator[](source).emplace_back(to, cost);
         if (!UnWeightedGraph<T>::is_directed_graph) UnWeightedGraph<T>::operator[](to).emplace_back(source, cost);
     }
