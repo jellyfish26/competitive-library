b860df159237db76c21b095cfcf7045e5bc949d3
jellyfish26 <jellyfish0162@gmail.com>
2020-05-11 02:42:05 +0900

    update Krsukal (bug fix)

--- graph/Kruskal.cpp
+++ graph/Kruskal.cpp
@@ -3,7 +3,7 @@
 #include "../datastructure/UnionFind.cpp"
 using namespace std;
 
-// Last updated 2020-05-09
+// Last updated 2020-05-11
 template<typename T>
 class Kruskal {
 private:
@@ -12,8 +12,8 @@ private:
     T kruskal_cost;
 
     void convert_graph_to_edges(WeightedGraph<T> &graph) {
-        for (auto &vertex_edges : graph) {
-            for (auto &temp : vertex_edges) edges.push_back(temp);
+        for (size_t index = 0; index < graph.size(); index++) {
+            for (auto &temp : graph[index]) edges.push_back(temp);
         }
     }
 
@@ -26,7 +26,7 @@ public:
 
     void build() {
         kruskal_cost = 0;
-        sort(edges.begin(), edges.end(), [](edge<T> left, edge<T> right) {
+        sort(edges.begin(), edges.end(), [](edge<T> &left, edge<T> &right) {
             return left.cost < right.cost;
         });
         UnionFind uf(vertex_size);
