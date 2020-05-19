258fe9ad9db7f19cf2595837ee688542b02a3c1c
jellyfish26 <jellyfish0162@gmail.com>
2020-05-20 02:05:12 +0900

    update BIT (implement lower bound and upper bound)

--- datastructure/BinaryIndexedTree.cpp
+++ datastructure/BinaryIndexedTree.cpp
@@ -1,11 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Last updated 2020-4-22
+// Last updated 2020-05-20
 template<typename T>
 class BinaryIndexedTree {
 private:
     vector<T> data;
+
+    inline size_t binary_search(const T value, const bool is_contain) {
+        size_t low = 0, high = data.size();
+        while (high - low > 1) {
+            size_t middle = low + (high - low) / 2;
+            if (sum(middle) < value + is_contain) low = middle;
+            else high = middle;
+        }
+        return high;
+    }
 
 public:
     explicit BinaryIndexedTree(size_t data_size) : data(data_size + 1, 0) {}
@@ -13,7 +23,7 @@ public:
     void update(size_t index, T value) {
         for (size_t now_index = index + 1; now_index < data.size(); now_index += (now_index & -now_index)) {
             data[now_index] += value;
-        } 
+        }
     }
 
     T sum(size_t index) {
@@ -29,7 +39,18 @@ public:
     }
 
     // [left, right)
-    T query(int left_index, int right_index) {
-        return sum(right_index - 1) - sum(left_index - 1);
+    T query(size_t left_index, size_t right_index) {
+        T ret = 0;
+        ret += (right_index == 0 ? 0 : sum(right_index - 1));
+        ret += (left_index == 0 ? 0 : sum(left_index - 1));
+        return ret;
+    }
+
+    size_t lower_bound(T value) {
+        return binary_search(value, false);
+    }
+
+    size_t upper_bound(T value) {
+        return binary_search(value, true);
     }
 };
