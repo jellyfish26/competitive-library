ab25c694db01a98d8520a1fe9de63804fc28eb5b
jellyfish26 <jellyfish0162@gmail.com>
2021-02-19 04:55:29 +0900

    Refactoring base of Graph

---
+++ graph/Base.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#include <vector>
+
+// Last Update 2021-02-19
+template<typename T>
+class Edge {
+public:
+    int src, to;
+    T cost;
+
+    Edge(int src, int to, T cost);
+    Edge(int to, T cost);
+    Edge(const Edge &target);
+
+    edge &operator=(const int &vt);
+
+    operator int() const { return to; }
+};
+
+template<typename T>
+Edge<T>::Edge(int src, int to, T cost) : src(src), to(to), cost(cost) {}
+
+template<typename T>
+Edge<T>::Edge(int to, T cost) : src(-1), to(to), cost(cost) {}
+
+template<typename T>
+Edge<T>::Edge(const Edge<T> &target) : src(target.src), to(target.to), cost(target.cost) {}
+
+template<typename T>
+class WeightedGraph {
+public:
+    std::vector<std::vector<Edge<T>>> graph;
+    const bool is_directed;
+
+    WeightedGraph(std::size_t size, bool is_directed);
+    
+    void add_edge(int src, int to, T cost);
+
+    const std::vector<Edge<T>> &operator[](std::size_t index) const;
+    std::size_t size() const;    
+};
+
+template<typename T>
+WeightedGraph<T>::WeightedGraph(std::size_t size, bool is_directed) : data(size), is_directed(is_directed) {}
+
+template<typename T>
+void WeightedGraph<T>::add_edge(int src, int to, T cost) {
+    graph[src].emplce_back(src, to, cost);
+    if (!is_directed) {
+        graph[to].emplace_back(to, src, cost);
+    }
+}
+
+template<typename T>
+const std::vector<Edge<T>> &WeightedGraph<T>::operator[](std::size_t index) const{
+    return graph[index];
+}
+
+template<typename T>
+std::size_t WeightedGraph<T>::size() const {
+    return graph.size();
+}
