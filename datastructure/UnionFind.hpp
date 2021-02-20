a4954bea183850a2c408972fe5d224ab14dbd76a
jellyfish26 <jellyfish0162@gmail.com>
2021-02-20 19:30:08 +0900

    Add Unionfind source file

--- datastructure/UnionFind.hpp
+++ datastructure/UnionFind.hpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <utility>
 
-// Last Update 2021-02-17
+// Last Update 2021-02-20
 class UnionFind {
 private:
     std::vector<int> data;
@@ -15,27 +15,3 @@ public:
     int size(int x);
     bool same(int x, int y);
 };
-
-UnionFind::UnionFind(std::size_t data_size) : data(data_size, -1)  {}
-
-bool UnionFind::unite(int x, int y) {
-    x = find(x), y = find(y);
-    if(x == y) return false;
-    if(data[x] > data[y]) std::swap(x, y);
-    data[x] += data[y];
-    data[y] = x;
-    return true;
-}
-
-int UnionFind::find(int x) {
-    if (data[x] < 0) return x;
-    else return (data[x] = find(data[x]));
-}
-
-int UnionFind::size(int x) {
-    return (-data[find(x)]);
-}
-
-bool UnionFind::same(int x, int y) {
-    return find(x) == find(y);
-}
